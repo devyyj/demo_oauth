513d772fa5dd6913ce5ddc44480ab9c5de919834
devyyj <devyyj@gmail.com>
2023-08-23 16:36:58 +0900

    테스트 중

--- src/main/java/com/example/demo_oauth/MainController.java
+++ src/main/java/com/example/demo_oauth/MainController.java
@@ -1,17 +1,18 @@
 package com.example.demo_oauth;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
 public class MainController {
     @GetMapping
-    public String root(){
+    public String root(Authentication authentication){
         return "root!";
     }
 
     @GetMapping("/api/test")
-    public String api(){
+    public String api(Authentication authentication){
         return "/api/test!";
     }
 }
--- src/main/java/com/example/demo_oauth/security/service/MyDefaultOAuth2UserService.java
+++ src/main/java/com/example/demo_oauth/security/service/MyDefaultOAuth2UserService.java
@@ -1,34 +1,34 @@
-package com.example.demo_oauth.security.service;
-
-import com.example.demo_oauth.security.repository.MyUserRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
-
-@Log4j2
-@Service
-@RequiredArgsConstructor
-public class MyDefaultOAuth2UserService extends DefaultOAuth2UserService {
-
-    private final MyUserRepository userRepository;
-
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-
-//        String sub = user.getAttribute("sub");
+//package com.example.demo_oauth.security.service;
 //
-//        if(userRepository.findBySocialId(sub).isEmpty()) {
-//            userRepository.save(MyUser.builder().nickName(sub).socialId(sub).build());
-//        }
+//import com.example.demo_oauth.security.repository.MyUserRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+//import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+//import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.stereotype.Service;
 //
-//        log.info("======================================");
-//        log.info(user.toString());
-//        log.info("======================================");
-
-        return super.loadUser(userRequest);
-    }
-}
+//@Log4j2
+//@Service
+//@RequiredArgsConstructor
+//public class MyDefaultOAuth2UserService extends DefaultOAuth2UserService {
+//
+//    private final MyUserRepository userRepository;
+//
+//    @Override
+//    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+//
+////        String sub = user.getAttribute("sub");
+////
+////        if(userRepository.findBySocialId(sub).isEmpty()) {
+////            userRepository.save(MyUser.builder().nickName(sub).socialId(sub).build());
+////        }
+////
+////        log.info("======================================");
+////        log.info(user.toString());
+////        log.info("======================================");
+//
+//        return super.loadUser(userRequest);
+//    }
+//}
